0057465685b5ac4ce9592891aad9194b0a729d13
Keith Wong <keith.wong3672@gmail.com>
2016-06-30 21:41:26 -0700

    Make all bare-minimum tests pass

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -10,11 +10,52 @@
 
 var fs = require('fs');
 var Promise = require('bluebird');
+var request = require('request');
 
 
 
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
-  // TODO
+  return new Promise(function(resolve, reject) {
+    fs.readFile(readFilePath, (err, data) => {
+      if (!err) {
+        var userName = data.toString().split('\n')[0];
+        resolve(userName);
+      } else {
+        reject(err);
+      }
+    });
+  }).then( function(user) {
+    return new Promise(function(resolve, reject) {
+      var options = {
+        url: 'https://api.github.com/users/' + user,
+        headers: { 'User-Agent': 'request' },
+        json: true  // will JSON.parse(body) for us
+      };
+
+      request.get(options, function(err, res, body) {
+        if (err) {
+          reject(err);
+        } else if (body.message) {
+          reject(new Error('Failed to get GitHub profile: ' + body.message), null);
+        } else {
+          resolve(body);
+        }
+      });
+    });
+  }).then(function(profile) {
+    return new Promise(function(resolve, reject) {
+      var saveThis = JSON.stringify(profile);
+      fs.writeFile(writeFilePath, saveThis, function (err) {
+        if (err) {
+          console.log('line 54 error');
+        } else {
+          resolve();
+        }
+      });
+      
+    });
+
+  });
 };
 
 // Export these functions so we can test them
